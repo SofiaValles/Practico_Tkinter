0a62b316802d8314991aced24a0f7fb82b258cef
Sofía Valles <168451567+SofiaValles@users.noreply.github.com>
2024-08-28 10:07:26 -0300

    Mejora interfaz Ejercicio_8_Tkinter_Calculadora_2.py
    Se achicó la interfaz y se modificó para que sea más estética

--- Ejercicio_8_Tkinter_Calculadora_2.py
+++ Ejercicio_8_Tkinter_Calculadora_2.py
@@ -47,7 +47,7 @@ def Calcular():
 # Ventana principal
 ventana = Tk()
 ventana.title("Calculadora 2")
-ventana.geometry("660x280")
+ventana.geometry("460x280")
 ventana.configure(bg="#F2D5E8")
 ventana.resizable(False, False)
 
@@ -55,33 +55,33 @@ validate_cmd = ventana.register(Validar_entrada)
 
 # Entradas de valores
 entry = Entry(ventana, bg="white", validate="key", validatecommand=(validate_cmd, '%S'))
-entry.place(x=200, y=10, width=130, height=20)
+entry.place(x=80, y=10, width=130, height=20)
 
 entry2 = Entry(ventana, bg="white", validate="key", validatecommand=(validate_cmd, '%S'))
-entry2.place(x=200, y=60, width=130, height=20)
+entry2.place(x=80, y=60, width=130, height=20)
 
 entry_res = Entry(ventana, bg="#C1C9D9", state='readonly')
-entry_res.place(x=200, y=110, width=130, height=20)
+entry_res.place(x=80, y=110, width=130, height=20)
 
 # Variable
 operacion_var = IntVar()
 
 # RadioButtons 
 button_suma = Radiobutton(ventana, text="Sumar", variable=operacion_var, value=1, borderwidth=2, height=2, width=8, bg="#C1C9D9", font=('Comic sens MC', 8, 'bold'))
-button_suma.place(x=500, y=50)
+button_suma.place(x=300, y=50)
 
 button_rest = Radiobutton(ventana, text="Restar", variable=operacion_var, value=2, borderwidth=2, height=2, width=8, bg="#C1C9D9", font=('Comic sens MC', 8, 'bold'))
-button_rest.place(x=500, y=100)
+button_rest.place(x=300, y=100)
 
 button_mult = Radiobutton(ventana, text="Multiplicar", variable=operacion_var, value=3, borderwidth=2, height=2, width=8, bg="#C1C9D9", font=('Comic sens MC', 8, 'bold'))
-button_mult.place(x=500, y=150)
+button_mult.place(x=300, y=150)
 
 button_div = Radiobutton(ventana, text="Dividir", variable=operacion_var, value=4, borderwidth=2, height=2, width=8, bg="#C1C9D9", font=('Comic sens MC', 8, 'bold'))
-button_div.place(x=500, y=200)
+button_div.place(x=300, y=200)
 
 # Botón 
 button_calcular = Button(ventana, text="Calcular", borderwidth=2, height=2, width=10, bg="#F26D91", font=('Comic sens MC', 11, 'bold'), command=Calcular)
-button_calcular.place(x=215, y=160)
+button_calcular.place(x=115, y=160)
 
 # Etiquetas 
 label = Label(ventana, text="Valor 1", bg="#F2D5E8", fg="#785330", font=('Impact', 12))
@@ -94,6 +94,6 @@ label_res = Label(ventana, text="Resultado", bg="#F2D5E8", fg="#785330", font=('
 label_res.place(x=20, y=110, width=100, height=20)
 
 label_op = Label(ventana, text="Operaciones", bg="#F2D5E8", fg="#A64141", font=('Impact', 16))
-label_op.place(x=470, y=10, width=150, height=20)
+label_op.place(x=270, y=10, width=150, height=20)
 
 ventana.mainloop()
