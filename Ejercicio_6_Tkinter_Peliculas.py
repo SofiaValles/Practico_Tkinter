20748692f19a4e02ef3861c7c4dc530c9ddfe089
Sofía Valles <168451567+SofiaValles@users.noreply.github.com>
2024-09-15 20:53:47 -0300

    Update Ejercicio_6_Tkinter_Peliculas.py
    Se agregan validaciones de espacios

--- Ejercicio_6_Tkinter_Peliculas.py
+++ Ejercicio_6_Tkinter_Peliculas.py
@@ -2,25 +2,43 @@
 from tkinter import *
 
 def Agregar():
-    peli = entry.get()
+    peli = entry_var.get()
+    caracteres = list(peli)
 
-    if peli in lista.get(0, END):
-        modal = Toplevel(ventana)
-        modal.title("Película ya ingresada")
-        modal.geometry("300x150")
-        modal.configure(bg="#f5eb9d")
+    if "  " in peli or caracteres[0] == " ":
+        entry_var.set("")
+        modal_espacios = Toplevel(ventana)
+        modal_espacios.title("Error")
+        modal_espacios.geometry("300x150")
+        modal_espacios.configure(bg="#f5eb9d")
 
-        label_error= Label(modal, text="¡Error!",bg="#f5eb9d", fg="#4b1854", font=('Impact', 12))
-        label_error.place(x=30, y=15, width=140, height=35)
-        label_error2= Label(modal, text="Su película ya ha sido cargada",bg="#f5eb9d", fg="#593822", font=('Times New Roman', 12))
-        label_error2.place(x=40, y=25, width=220, height=35)
+        label_error= Label(modal_espacios, text="No puede ingresar tantos espacios!",bg="#f5eb9d", fg="#4b1854", font=('Comic sens MC', 12))
+        label_error.place(x=25 , y=25, width=250, height=35)
 
-        boton_cerrar= Button(modal,text="Aceptar",borderwidth=2, height=1, width=8,anchor="center",bg="#DAA38F", font= ('Comic sens MC',8,'bold'), command=modal.destroy)
-        boton_cerrar.place(x=110, y=80)
+        boton_cerrar_espacios= Button(modal_espacios,text="Aceptar",borderwidth=2, height=1, width=8,anchor="center",bg="#DAA38F", font= ('Comic sens MC',8,'bold'), command=modal_espacios.destroy)
+        boton_cerrar_espacios.place(x=110, y=80)
+        
+        
 
+        if peli in lista.get(0, END):
+            modal = Toplevel(ventana)
+            modal.title("Error")
+            modal.geometry("300x150")
+            modal.configure(bg="#f5eb9d")
+            
+            label_error2= Label(modal, text="Su película ya ha sido cargada",bg="#f5eb9d", fg="#593822", font=('Times New Roman', 12))
+            label_error2.place(x=40, y=25, width=220, height=35)
+
+            boton_cerrar= Button(modal,text="Aceptar",borderwidth=2, height=1, width=8,anchor="center",bg="#DAA38F", font= ('Comic sens MC',8,'bold'), command=modal.destroy)
+            boton_cerrar.place(x=110, y=80)
+            entry_var.set("")
     else:
         lista.insert(END, peli)
-        entry.delete(0, END)
+        entry_var.set("")
+        
+
+    
+
 #ventana
 ventana = Tk()
 ventana.title("Películas")
@@ -33,7 +51,8 @@ button=Button(ventana,text="Añadir",borderwidth=2, height=1, width=8,anchor="ce
 button.place(x=93, y=147)
 
 #Entrada
-entry = Entry(ventana, bg="#ccc3fa")
+entry_var = StringVar(value="")
+entry = Entry(ventana, bg="#ccc3fa",textvariable=entry_var)
 entry.place(x=30, y=110, width=200, height=20)
 
 #Lista películas
